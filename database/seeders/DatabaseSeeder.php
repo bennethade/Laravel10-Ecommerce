8e734d75db0cc1856572b42b714456871ef4d64d
bennethade <92984648+bennethade@users.noreply.github.com>
2023-10-05 10:08:02 +0100

    single project page added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +22,12 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+
+        Brand::factory(6)->create();
+        Category::factory(6)->create();
+        Product::factory(24)->create();
+
+
     }
 }
